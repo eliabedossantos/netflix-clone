3e3149abad32ee1a86638ac2175bbf672b3ad393
eliabedossantos <eliabe.s.o36@gmail.com>
2021-03-20 00:39:08 -0300

    add dropdown notify

--- assets/js/setup.js
+++ assets/js/setup.js
@@ -15,16 +15,33 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
-//DROPDOWN
-$("#grupos").hover(function(){
-    $("#conteudo").show(); //quando clica no input mostra o conteudo
+
+//DROPDOWN PROFILE
+$("#groups1").hover(function(){
+    $("#content1").show(); //quando clica no input mostra o conteudo
+    $("#content2").hide();
   });
   
-  $("body, #aplicar").click(function(){
-    $("#conteudo").hide(); //quando aplica ou clica fora esconde
+  $("body, #apply1").click(function(){
+    $("#content1").hide(); //quando aplica ou clica fora esconde
   });
   
   //este é preciso também para não esconder quando clica no grupos ou conteudo
-  $("#grupos, #conteudo").click(function(e){
+  $("#groups1, #content1").click(function(e){
+    e.stopPropagation(); 
+  });
+
+ // DROPDOWN NOTIFY
+ $("#groups2").hover(function(){
+    $("#content2").show(); //quando clica no input mostra o conteudo
+    $("#content1").hide();
+  });
+  
+  $("body, #apply2").click(function(){
+    $("#content2").hide(); //quando aplica ou clica fora esconde
+  });
+  
+  //este é preciso também para não esconder quando clica no grupos ou conteudo
+  $("#groups2, #content2").click(function(e){
     e.stopPropagation(); 
   });
