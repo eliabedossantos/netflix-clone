9c5a8ca5e6e382f5d9c2efd33fbfc104673174f9
eliabedossantos <eliabe.s.o36@gmail.com>
2021-03-20 23:04:01 -0300

    Add textbox animation

--- assets/js/setup.js
+++ assets/js/setup.js
@@ -15,7 +15,6 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
-
 //DROPDOWN PROFILE
 $("#groups1").hover(function(){
     $("#content1").show(); //quando clica no input mostra o conteudo
@@ -25,23 +24,32 @@ $("#groups1").hover(function(){
   $("body, #apply1").click(function(){
     $("#content1").hide(); //quando aplica ou clica fora esconde
   });
-  
   //este é preciso também para não esconder quando clica no grupos ou conteudo
   $("#groups1, #content1").click(function(e){
     e.stopPropagation(); 
   });
-
  // DROPDOWN NOTIFY
  $("#groups2").hover(function(){
-    $("#content2").show(); //quando clica no input mostra o conteudo
+    $("#content2").show(); 
     $("#content1").hide();
   });
   
   $("body, #apply2").click(function(){
-    $("#content2").hide(); //quando aplica ou clica fora esconde
+    $("#content2").hide();
   });
-  
-  //este é preciso também para não esconder quando clica no grupos ou conteudo
   $("#groups2, #content2").click(function(e){
     e.stopPropagation(); 
   });
+  // SEARCH
+  var widthsrc = document.querySelector(".searchinput");
+  function verMais(){
+      if(widthsrc.style.width == "0px"){
+        widthsrc.style.width = "240px";
+        document.getElementById("search").style.border = "1px solid #fff";
+       
+    }else{
+        widthsrc.style.width = "0px";
+        document.getElementById("search").style.border = "none";
+    }
+}
+
